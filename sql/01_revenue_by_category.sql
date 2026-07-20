7a9ad83d30fcc89deeced4ec7ccb6b1ad9983306
Sienna-F <wenqing.ff@gmail.com>
2026-07-20 10:30:25 +0800

    Create 01_revenue_by_category.sql

---
+++ sql/01_revenue_by_category.sql
@@ -0,0 +1,26 @@
+/*
+==========================================================
+Project
+----------------------------------------------------------
+Global Commercial Performance Dashboard
+
+Analysis
+----------------------------------------------------------
+01 - Revenue by Category
+
+Business Question
+----------------------------------------------------------
+Which product categories generate the highest revenue,
+and how concentrated is revenue across product categories?
+
+Business Value
+----------------------------------------------------------
+Understanding revenue contribution by category helps
+identify core business drivers, evaluate portfolio
+concentration, and support future investment decisions.
+
+==========================================================
+*/
+
+
+-- SQL Query
